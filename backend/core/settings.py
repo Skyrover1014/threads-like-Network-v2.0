b8a3b6580dfcb87c52d919912b0da76fe51e50d2
skyrover <maywin4755@gmail.com>
2025-07-14 11:13:40 +0800

    修正db配置

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -111,18 +111,27 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'threads',
+#         'USER': 'postgres',
+#         'PASSWORD': '123456',
+#         'HOST': 'db',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'threads',
-        'USER': 'postgres',
-        'PASSWORD': '123456',
-        'HOST': 'db',
-        'PORT': '5432',
+        'NAME': os.getenv('DATABASE_NAME', 'threads'),
+        'USER': os.getenv('DATABASE_USER', 'postgres'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD', '123456'),
+        'HOST': os.getenv('DATABASE_HOST', 'db'),  # ✅ 預設是 docker-compose 的 service 名稱
+        'PORT': os.getenv('DATABASE_PORT', '5432'),
     }
 }
-
-
 
 
 # Password validation
